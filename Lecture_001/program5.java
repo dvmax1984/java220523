0d4abbcd82ef5188b7052f217bab5a8e7dbd78a2
Dvortsov Maksim <maximgsm@gmail.com>
2023-06-22 22:00:44 +0700

    Методы

--- Lecture_001/program5.java
+++ Lecture_001/program5.java
@@ -1,5 +1,4 @@
 package Lecture_001;
-
 public class program5 {
     public static void main(String[] args) {
         int a = 1, b = 2;
---
+++ Lecture_001/program6.java
@@ -0,0 +1,12 @@
+package Lecture_001;
+public class program6 {
+    public static void main(String[] args) {
+        float pi = 3.1415f;
+        System.out.printf("%f\n", pi); // 3,141500
+        System.out.printf("%.2f\n", pi); // 3,14
+        System.out.printf("%.3f\n", pi); // 3,141
+        System.out.printf("%e\n", pi); // 3,141500e+00
+        System.out.printf("%.2e\n", pi); // 3,14e+00
+        System.out.printf("%.3e\n", pi); // 3,141e+00
+    }
+}
