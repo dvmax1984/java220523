3881c51aec91daa08e689605b731b718ec23b9c3
Dvortsov Maksim <maximgsm@gmail.com>
2023-06-22 21:40:00 +0700

    L1

---
+++ Lecture_001/program5.java
@@ -0,0 +1,11 @@
+package Lecture_001;
+
+public class program5 {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+        int c = a + b;
+        String res = String.format("%d + %d = %d \n", a, b, c);
+        System.out.printf("%d + %d = %d \n", a, b, c);
+        System.out.println(res);
+    }
+}
