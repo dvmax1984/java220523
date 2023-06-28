428ef0115fb7990b79c99f33b4c73b20c6c7db93
Dvortsov Maksim <maximgsm@gmail.com>
2023-06-29 00:36:38 +0700

    Add Seminar 2

--- Homework/homework_002/program.java
+++ Homework/homework_002/program.java
@@ -1,5 +1,79 @@
-package Homework.homework_002;
+package Homework.Hhomework_002;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+// 1) Напишите метод, который определит тип (расширение) файлов из текущей папки и выведет в консоль результат вида
+// 1 Расширение файла: txt
+// 2 Расширение файла: pdf
+// 3 Расширение файла:
+// 4 Расширение файла: jpg
 public class program {
+    public static void main(String[] args) throws SecurityException, IOException {
+        
+        String pathProject = System.getProperty("user.dir");
+        String pathFile1 = pathProject.concat("\\Seminar2-1.txt");
+        String pathFile2 = pathProject.concat("\\Seminar2-2.txt");
+        //addingToFile(pathFile, longStringCreator("Hello!", 5));
+        addListToFile(pathFile2, showCurrentFiles(pathProject));        
+    }
+
+    static String longStringCreator(String word, int iters) {
+
+        StringBuilder newStr = new StringBuilder();
+        
+        for (int i = 0; i < iters; i++) {
+            newStr.append(word);
+        }
+        return newStr.toString() + "\n";
+    }
+
+    static String[] showCurrentFiles(String dirPath) {
+            File currentDir = new File(dirPath);
+            return currentDir.list();
+        }
+        
+        static void addingToFile(String filePath, String text) {
+    try {
+        File myFile = new File(filePath);
+        FileWriter writer = new FileWriter(myFile, true);
+
+        writer.write(text);
+        writer.close();
+        } 
+        catch (Exception e) {
+        System.out.println(e.getMessage());
+        }
+    }
+
+    static void addListToFile(String filePath, String[] list) throws SecurityException, IOException {
+    Logger logger = Logger.getLogger(program.class.getName());
+    FileHandler fh = new FileHandler("Seminar2-3(log).txt");
+
+    logger.addHandler(fh);
+
+    SimpleFormatter simple = new SimpleFormatter();
+    fh.setFormatter(simple);
+
+    try {
+        File myFile = new File(filePath);
+
+        FileWriter writer = new FileWriter(myFile);
+
+        for (String str : list) {
+            writer.write(str + "\n");
+        }
+
+        writer.close();
+
+    } catch (Exception e) {
+        logger.warning(e.getMessage());
+    }
+
+}
     
 }
--- Seminar/Seminar_002/Program.java
+++ Seminar/Seminar_002/Program.java
@@ -1,29 +1,79 @@
 package Seminar.Seminar_002;
 
-public class Program {
-    public static void main(String[] args) {
-        System.out.println(longStringBuilding(10, 'a', 'b'));
-        System.out.println(isPalindrom("qwwq"));
-    }
-    
-    static String longStringBuilding(int n, char c1, char c2) {
-        // BaHo 4eTHoe uncno N (>@) u cumeonsi cl u c2.
-        // HanucaTb MeTOA, KOTOpsIi BEpHET CTPOKY AMHb N, KOTOpas cocTom
-        // waumHas c cl.
-        StringBuilder newStr = new StringBuilder();
-        for (int i = 0; i < n / 2; i++) {
-            newStr.append(c1);
-            newStr.append(c2);
-        }
-        return newStr.toString();
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+// 1) Напишите метод, который определит тип (расширение) файлов из текущей папки и выведет в консоль результат вида
+// 1 Расширение файла: txt
+// 2 Расширение файла: pdf
+// 3 Расширение файла:
+// 4 Расширение файла: jpg
+public class program {
+    public static void main(String[] args) throws SecurityException, IOException {
+        
+        String pathProject = System.getProperty("user.dir");
+        String pathFile1 = pathProject.concat("\\Seminar2-1.txt");
+        String pathFile2 = pathProject.concat("\\Seminar2-2.txt");
+        //addingToFile(pathFile, longStringCreator("Hello!", 5));
+        addListToFile(pathFile2, showCurrentFiles(pathProject));        
     }
 
-    static boolean isPalindrom(String str) {
-        StringBuilder res = new StringBuilder("");
+    static String longStringCreator(String word, int iters) {
+
+        StringBuilder newStr = new StringBuilder();
         
-        for (int i = str.length() - 1; i >= 0; i--)
-            res.append(str.charAt(i));
-            
-        return str.equals(res.toString());
+        for (int i = 0; i < iters; i++) {
+            newStr.append(word);
+        }
+        return newStr.toString() + "\n";
     }
+
+    static String[] showCurrentFiles(String dirPath) {
+            File currentDir = new File(dirPath);
+            return currentDir.list();
+        }
+        
+        static void addingToFile(String filePath, String text) {
+    try {
+        File myFile = new File(filePath);
+        FileWriter writer = new FileWriter(myFile, true);
+
+        writer.write(text);
+        writer.close();
+        } 
+        catch (Exception e) {
+        System.out.println(e.getMessage());
+        }
+    }
+
+    static void addListToFile(String filePath, String[] list) throws SecurityException, IOException {
+    Logger logger = Logger.getLogger(program.class.getName());
+    FileHandler fh = new FileHandler("Seminar2-3(log).txt");
+
+    logger.addHandler(fh);
+
+    SimpleFormatter simple = new SimpleFormatter();
+    fh.setFormatter(simple);
+
+    try {
+        File myFile = new File(filePath);
+
+        FileWriter writer = new FileWriter(myFile);
+
+        for (String str : list) {
+            writer.write(str + "\n");
+        }
+
+        writer.close();
+
+    } catch (Exception e) {
+        logger.warning(e.getMessage());
+    }
+
+}
+    
 }
